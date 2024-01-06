7534adfb28a462f8bb7550d12240e3310b0d7e63
Stephen Oladele <stephen@writewithneurl.com>
2024-01-06 11:24:13 +0000

    Deployed `mistral_prompt`

---
+++ deployments/mistral_prompt/source.py
@@ -0,0 +1,10 @@
+import modelbit, sys
+from typing import *
+
+# main function
+def mistral_prompt(prompt):
+    return {'output': 'Hello World!'}
+
+# to run locally via git & terminal, uncomment the following lines
+# if __name__ == "__main__":
+#   print(mistral_prompt(*(modelbit.parseArg(v) for v in sys.argv[1:])))
