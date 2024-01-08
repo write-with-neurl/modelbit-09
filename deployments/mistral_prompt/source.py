9f4c3c4fb8f0e142b4b3f9c0b1c0ce79af0fa450
stephen-encord <stephen@encord.com>
2024-01-08 19:39:30 +0100

    Add requriements and source files

--- deployments/mistral_prompt/source.py
+++ deployments/mistral_prompt/source.py
@@ -1,9 +1,23 @@
 import modelbit, sys
+import cloudpickle
 from typing import *
 
+
+with open('pipe-int4.pkl', 'rb') as file:
+    pipe = cloudpickle.load(file)
+    
 # main function
 def mistral_prompt(prompt):
-    return {'output': 'Hello World!'}
+    sequences = pipe(
+        prompt,
+        do_sample=False,
+        max_new_tokens=300,
+        temperature=0.7,
+        top_k=50,
+        top_p=0.95,
+        num_return_sequences=1,
+    )
+    return {'output': sequences[0]['generated_text']}
 
 # to run locally via git & terminal, uncomment the following lines
 # if __name__ == "__main__":
